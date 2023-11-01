248d81f1ecab4c852a3d2c6aa9d4a7f944660c30
Hadi <hadibakhshi277@gmail.com>
2023-11-01 13:35:02 +0330

    feat: configure logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,8 +8,8 @@ import { LoggerModule } from 'nestjs-pino';
 import { ApplicationModule } from './application/application.module';
 import { InfrastructureModule } from './infrastructure/infrastructure.module';
 import { PresentationModule } from './presentation/presentation.module';
-// import { randomUUID } from 'node:crypto';
-// import pino from 'pino';
+import { randomUUID } from 'node:crypto';
+import pino from 'pino';
 
 @Module({
   imports: [
@@ -29,28 +29,28 @@ import { PresentationModule } from './presentation/presentation.module';
     ]),
     LoggerModule.forRoot({
       pinoHttp: {
-        // stream: pino.destination({
-        //   dest: './logs',
-        //   minLength: 4096,
-        //   sync: false,
-        // }),
-        // genReqId: function (req, res) {
-        //   const existingID = req.id ?? req.headers['x-request-id'];
-        //   if (existingID) return existingID;
-        //   const id = randomUUID();
-        //   res.setHeader('X-Request-Id', id);
-        //   return id;
-        // },
-        // customLogLevel(req, res, error) {
-        //   if (res.statusCode >= 400 && res.statusCode < 500) {
-        //     return 'warn';
-        //   } else if (res.statusCode >= 500 || error) {
-        //     return 'error';
-        //   } else if (res.statusCode >= 300 && res.statusCode < 400) {
-        //     return 'silent';
-        //   }
-        //   return 'info';
-        // },
+        stream: pino.destination({
+          dest: './logs',
+          minLength: 4096,
+          sync: false,
+        }),
+        genReqId: function (req, res) {
+          const existingID = req.id ?? req.headers['x-request-id'];
+          if (existingID) return existingID;
+          const id = randomUUID();
+          res.setHeader('X-Request-Id', id);
+          return id;
+        },
+        customLogLevel(req, res, error) {
+          if (res.statusCode >= 400 && res.statusCode < 500) {
+            return 'warn';
+          } else if (res.statusCode >= 500 || error) {
+            return 'error';
+          } else if (res.statusCode >= 300 && res.statusCode < 400) {
+            return 'silent';
+          }
+          return 'info';
+        },
       },
     }),
     ApplicationModule,
