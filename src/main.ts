c1a113412e8284e27917500c395e8b6230a45677
Hadi <hadibakhshi277@gmail.com>
2023-11-01 13:35:55 +0330

    feat: add global exception handler
    add a filter that will catch all the exceptions

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as compression from 'compression';
 import helmet from 'helmet';
 import { Logger } from 'nestjs-pino';
 
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './presentation/filters/allExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
-
+  const httpAdapterHost = app.get(HttpAdapterHost);
   // ----------------------------- Middlewares --------------------------------------
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
@@ -24,7 +25,7 @@ async function bootstrap() {
   app.enableVersioning({
     type: VersioningType.URI,
   });
-
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
   // ----------------------------- Swagger Configuration --------------------------------------
   const config = new DocumentBuilder()
     .setTitle('Gooyeh Back-end Application')
---
+++ src/presentation/filters/allExceptionFilter/allException.filter.ts
@@ -0,0 +1,40 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    // In certain situations `httpAdapter` might not be available in the
+    // constructor method, thus we should resolve it here.
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const ctx = host.switchToHttp();
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const responseMessage =
+      exception instanceof HttpException ? exception.getResponse() : '';
+
+    const responseBody = {
+      statusCode: httpStatus,
+      timestamp: new Date().toLocaleString(),
+      message:
+        (exception as HttpException).message ||
+        (responseMessage as any).message,
+      path: httpAdapter.getRequestUrl(ctx.getRequest()),
+    };
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
+
