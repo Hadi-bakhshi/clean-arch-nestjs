f2d38b220c96798f2a9d57bbdc4f4d2ce40931ac
Hadi <hadibakhshi277@gmail.com>
2023-11-01 13:35:47 +0330

    chore: change exception to test filter

--- src/application/use-cases/userUseCases.ts
+++ src/application/use-cases/userUseCases.ts
@@ -1,11 +1,13 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class UserUseCases {
-  private readonly logger = new Logger(UserUseCases.name);
+  // private readonly logger = new Logger(UserUseCases.name);
   constructor() {}
 
   getUser() {
-    return 'ME';
+    throw new BadRequestException('Wrong Request');
+    // throw new NotFoundException('چیزی یافت نشد');
+    // return 'ME';
   }
 }
